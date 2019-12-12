38be0102f2f9fc60ec1a196ca148f94ecdbb8fb2
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2019-12-12 19:24:19 +0530

    renamed points
    What changes you have made?
    .....
    renamed startPoint to start and endPoint to end
    
    Why this change is necessary?
    ....
    to shorten variable names and making thme more meaningful
    
    What is the impact of the change?
    .....
    It will be easier to use shorten name

--- src/line.js
+++ src/line.js
@@ -1,19 +1,19 @@
 class Line {
   constructor(x1, y1, x2, y2) {
-    this.startPoint = { x: x1, y: y1 };
-    this.endPoint = { x: x2, y: y2 };
+    this.start = { x: x1, y: y1 };
+    this.end = { x: x2, y: y2 };
   }
   isEqual(anotherLine) {
-    const areX1sEqual = this.startPoint.x == anotherLine.startPoint.x;
-    const areX2sEqual = this.endPoint.x == anotherLine.endPoint.x;
-    const areY1sEqual = this.startPoint.y == anotherLine.startPoint.y;
-    const areY2sEqual = this.endPoint.y == anotherLine.endPoint.y;
+    const areX1sEqual = this.start.x == anotherLine.start.x;
+    const areX2sEqual = this.end.x == anotherLine.end.x;
+    const areY1sEqual = this.start.y == anotherLine.start.y;
+    const areY2sEqual = this.end.y == anotherLine.end.y;
     return areX1sEqual && areX2sEqual && areY1sEqual && areY2sEqual;
   }
   toString() {
     let strObj = "Line: ";
-    strObj += `(${this.startPoint.x}, ${this.startPoint.y}) to `;
-    strObj += `(${this.endPoint.x}, ${this.endPoint.y})`;
+    strObj += `(${this.start.x}, ${this.start.y}) to `;
+    strObj += `(${this.end.x}, ${this.end.y})`;
     return strObj;
   }
 }
