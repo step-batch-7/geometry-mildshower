9a02a69a87ce8fa92480681e6afd84d97c4d05cc
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2019-12-12 19:41:43 +0530

    Improved isEqual method of Line class
    What changes you have made?
    .....
    Added support for isEqual method of Line class so that it can invalided
    if object of another class or no class can be invalidated
    
    Why this change is necessary?
    ....
    to invalidate Objects form another class
    
    What is the impact of the change?
    .....

--- src/line.js
+++ src/line.js
@@ -3,11 +3,14 @@ class Line {
     [this.start, this.end] = [{ ...start }, { ...end }];
   }
   isEqual(anotherLine) {
+    const isLineObj = anotherLine instanceof Line;
     const areX1sEqual = this.start.x == anotherLine.start.x;
     const areX2sEqual = this.end.x == anotherLine.end.x;
     const areY1sEqual = this.start.y == anotherLine.start.y;
     const areY2sEqual = this.end.y == anotherLine.end.y;
-    return areX1sEqual && areX2sEqual && areY1sEqual && areY2sEqual;
+    return (
+      areX1sEqual && areX2sEqual && areY1sEqual && areY2sEqual && isLineObj
+    );
   }
   toString() {
     let strObj = "Line: ";
--- test/testLine.js
+++ test/testLine.js
@@ -1,25 +1,34 @@
 const assert = require("chai").assert;
 const Line = require("../src/line");
 
-describe("#isEqual()", () => {
-  it("should validate if equal lines are given", () => {
-    const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
-    const line2 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
-    assert.ok(line1.isEqual(line2));
+describe("Line", () => {
+  describe("#isEqual()", () => {
+    it("should validate if equal lines are given", () => {
+      const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
+      const line2 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
+      assert.ok(line1.isEqual(line2));
+    });
+
+    it("should invalidate if lines of unequal start or end points are given", () => {
+      const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
+      const line2 = new Line({ x: 5, y: 6 }, { x: 7, y: 8 });
+      assert.notOk(line1.isEqual(line2));
+    });
+
+    it("should invalidate if a object is given that is not an instance of Line class", () => {
+      const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
+      assert.notOk(
+        line1.isEqual({ start: { x: 1, y: 2 }, end: { x: 3, y: 4 } })
+      );
+    });
   });
 
-  it("should invalidate if unequal lines are given", () => {
-    const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
-    const line2 = new Line({ x: 5, y: 6 }, { x: 7, y: 8 });
-    assert.notOk(line1.isEqual(line2));
-  });
-});
-
-describe("#toString()", function() {
-  it("should give the string representation of the line object", function() {
-    const line = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
-    const actualValue = line.toString();
-    const expectedValue = "Line: (1, 2) to (3, 4)";
-    assert.strictEqual(actualValue, expectedValue);
+  describe("#toString()", function() {
+    it("should give the string representation of the line object", function() {
+      const line = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
+      const actualValue = line.toString();
+      const expectedValue = "Line: (1, 2) to (3, 4)";
+      assert.strictEqual(actualValue, expectedValue);
+    });
   });
 });
