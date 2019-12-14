f88c9f7d59033139a96073667101905f230a73df
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2019-12-14 11:29:35 +0530

    Added toString() method to Point class
    Now Point class has toString() method so Point objects can be
    represented in text using toString method

--- src/point.js
+++ src/point.js
@@ -2,6 +2,10 @@ class Point {
   constructor(x, y) {
     [this.x, this.y] = [x, y];
   }
+
+  toString() {
+    return `[Point @(${this.x},${this.y})]`;
+  }
 }
 
 module.exports = Point;
--- test/testPoint.js
+++ test/testPoint.js
@@ -1,4 +1,13 @@
 const assert = require("chai").assert;
 const Point = require("../src/point");
 
-point = new Point(1, 3);
+describe("Point", () => {
+  describe("#toString()", function() {
+    it("should give a text representation of the line", function() {
+      const point = new Point(4, 8);
+      const actualValue = point.toString();
+      const expectedValue = "[Point @(4,8)]";
+      assert.deepStrictEqual(actualValue, expectedValue);
+    });
+  });
+});
