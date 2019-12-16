eeaaf45ef3aa9eb5dad87c6b153fc78189d5bc33
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2019-12-16 14:15:56 +0530

    added isEqualTo method to circle
    isEqualTo method of circle takes a circle and states if center and
    radius of two circles are same or not
    if anything else other than circle is given then it also states false

--- src/circle.js
+++ src/circle.js
@@ -1,3 +1,9 @@
+const arePointsEqual = function(point1, point2) {
+  const areXsEqual = point1.x === point2.x;
+  const areYsEqual = point1.y === point2.y;
+  return areXsEqual && areYsEqual;
+};
+
 class Circle {
   constructor(center, radius) {
     this.center = { x: center.x, y: center.y };
@@ -7,6 +13,14 @@ class Circle {
   toString() {
     const center = `(${this.center.x},${this.center.y})`;
     return `[Circle @${center} radius ${this.radius}]`;
+  }
+
+  isEqualTo(other) {
+    return (
+      other instanceof Circle &&
+      this.radius === other.radius &&
+      arePointsEqual(this.center, other.center)
+    );
   }
 }
 
--- test/testCircle.js
+++ test/testCircle.js
@@ -10,4 +10,29 @@ describe("Circle", () => {
       assert.deepStrictEqual(actualValue, expectedValue);
     });
   });
+
+  describe("#isEqualTo()", function() {
+    it("should validate if two circles are have same center and same radius", function() {
+      const circle1 = new Circle({ x: 1, y: 2 }, 3);
+      const circle2 = new Circle({ x: 1, y: 2 }, 3);
+      assert.ok(circle1.isEqualTo(circle2));
+    });
+
+    it("should invalidate if two circles are have same center but different radius", function() {
+      const circle1 = new Circle({ x: 1, y: 2 }, 3);
+      const circle2 = new Circle({ x: 1, y: 2 }, 5);
+      assert.notOk(circle1.isEqualTo(circle2));
+    });
+
+    it("should invalidate if two circles are have different center and same radius", function() {
+      const circle1 = new Circle({ x: 1, y: 2 }, 3);
+      const circle2 = new Circle({ x: 1, y: 1 }, 3);
+      assert.notOk(circle1.isEqualTo(circle2));
+    });
+
+    it("should invalidate if given object is not a circle", function() {
+      const circle = new Circle({ x: 1, y: 2 }, 3);
+      assert.notOk(circle.isEqualTo({ center: { x: 1, y: 2 }, radius: 3 }));
+    });
+  });
 });
