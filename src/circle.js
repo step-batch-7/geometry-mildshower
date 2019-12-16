76e6f0813654a71653c2e337f83a19f7834aa7e4
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2019-12-16 14:01:12 +0530

    added toString method to circle
    toString method of circle gives a textual representation of the circle

--- src/circle.js
+++ src/circle.js
@@ -3,6 +3,11 @@ class Circle {
     this.center = { x: center.x, y: center.y };
     this.radius = radius;
   }
+
+  toString() {
+    const center = `(${this.center.x},${this.center.y})`;
+    return `[Circle @${center} radius ${this.radius}]`;
+  }
 }
 
 module.exports = Circle;
--- test/testCircle.js
+++ test/testCircle.js
@@ -1,4 +1,13 @@
 const assert = require("chai").assert;
 const Circle = require("../src/circle");
 
-const circle = new Circle({ x: 1, y: 2 }, 2);
+describe("Circle", () => {
+  describe("#toString()", function() {
+    it("should give string representation of the circle", function() {
+      const circle = new Circle({ x: 1, y: 2 }, 3);
+      const actualValue = circle.toString();
+      const expectedValue = "[Circle @(1,2) radius 3]";
+      assert.deepStrictEqual(actualValue, expectedValue);
+    });
+  });
+});
