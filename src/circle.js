5f3aa3a38cb11896482ceb867c8840e4daf830f2
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2019-12-16 13:56:19 +0530

    added circle module that has Circle constructor

---
+++ src/circle.js
@@ -0,0 +1,8 @@
+class Circle {
+  constructor(center, radius) {
+    this.center = { x: center.x, y: center.y };
+    this.radius = radius;
+  }
+}
+
+module.exports = Circle;
---
+++ test/testCircle.js
@@ -0,0 +1,4 @@
+const assert = require("chai").assert;
+const Circle = require("../src/circle");
+
+const circle = new Circle({ x: 1, y: 2 }, 2);
