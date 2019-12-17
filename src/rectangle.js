962b9c1d4d107f6ed8956151a6de65b83d722ab1
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2019-12-17 14:13:25 +0530

    refactored constructor of Rectangle class

--- src/rectangle.js
+++ src/rectangle.js
@@ -1,8 +1,17 @@
-const Line = require("./line");
+const Point = require("./point");
 
 class Rectangle {
   constructor(pointA, pointC) {
-    this.diagonal = new Line(pointA, pointC);
+    this.pointA = new Point(pointA.x, pointA.y);
+    this.pointB = new Point(pointA.x, pointC.y);
+    this.pointC = new Point(pointC.x, pointC.y);
+    this.pointD = new Point(pointC.x, pointA.y);
+    Object.defineProperties(this, {
+      pointA: { writable: false },
+      pointB: { writable: false },
+      pointC: { writable: false },
+      pointD: { writable: false }
+    });
   }
 }
 
